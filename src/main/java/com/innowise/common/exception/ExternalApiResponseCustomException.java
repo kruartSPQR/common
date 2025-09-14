21de2df2813314f79737bbab8f5b5b30c728fb84
lyber_spqr <artuom0405@gmail.com>
2025-09-14 20:55:18 +0300

    add ExternalApiResponseCustomException exception

---
+++ src/main/java/com/innowise/common/exception/ExternalApiResponseCustomException.java
@@ -0,0 +1,7 @@
+package com.innowise.common.exception;
+
+public class ExternalApiResponseCustomException extends RuntimeException {
+    public ExternalApiResponseCustomException(String message) {
+        super(message);
+    }
+}
