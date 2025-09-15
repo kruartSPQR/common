5b66b45da67546ee13fab4f229276dc38ee2498f
lyber_spqr <artuom0405@gmail.com>
2025-09-15 23:14:43 +0300

    add InvalidDateRangeCustomException

---
+++ src/main/java/com/innowise/common/exception/InvalidDateRangeCustomException.java
@@ -0,0 +1,7 @@
+package com.innowise.common.exception;
+
+public class InvalidDateRangeCustomException extends RuntimeException {
+    public InvalidDateRangeCustomException(String message) {
+        super(message);
+    }
+}
